666edcb99403a8759c6337c51ef2cf47660ad628
yhuxuf <yhuxuf@gmail.com>
2022-01-16 11:59:49 +0100

    added the user input feature

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main (String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the price you want to buy the shares");
         Stock abcCompany = new Stock();
         abcCompany.buy(36.6,2000);
         abcCompany.buy(39.7, 3000);
